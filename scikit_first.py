2b5346ab2a7e2a35844b58606a5a32598b5e3ac4
Cristian Copolutti <cristian.copolutti97@gmail.com>
2023-06-06 18:16:01 +0200

    primo esempio sickit ok

--- scikit_first.py
+++ scikit_first.py
@@ -49,8 +49,35 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     #X[:,0] --> prendo la prima colonna
     x1_min, x1_max = X[:,0].min() -1, X[:,0].max()+1
     x2_min, x2_max = X[:,1].min() -1, X[:,1].max()+1
-    #la funzione
+    #la funzione meshgrid viene utilizzata per creare una griglia di cordinate a partire da vettori di coordinate di input
     xx1,xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                           np.arange(x2_min, x2_max, resolution))
+    #la funzione ravel viene utilizzata per appiattire un array --> formato lineare
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=cl,
+                    edgecolor='black')
+
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:,0], X_test[:,1],
+                    edgecolor='black', alpha=1.0,
+                    linewidth=1, marker='o',
+                    s=100, label='test set')
+
+X_combined_std = np.vstack((X_train_scaled, X_test_scaled))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105,150))
+plt.xlabel('petal length [standardizzato]')
+plt.ylabel('petal width [standardizzato]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
